9874a9cf310decfdb0127c24f46261e1878ce4b2
fonzieyang <fonzieyang@gmail.com>
2017-08-18 01:59:41 +0800

    add login panel

---
+++ Project/Assets/Script/LogoLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoLogic : MonoBehaviour {
+	
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("GameScene2");
+        }
+	}
+}
